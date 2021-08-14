875348ff8f93aa75dae86fbeef1c03683d92620a
ItzKartik <smartzboi@gmail.com>
2021-08-14 18:46:53 +0530

    work

--- shuttle/urls.py
+++ shuttle/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from shuttle_app import views
 
 urlpatterns = [
+    re_path(r'^register/city/(?P<city_name>\w+)/$', views.register_form, name='register_form'),
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
--- shuttle_app/views.py
+++ shuttle_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+def register_form(request, city_name):
+    return render(request, 'register_form.html')
 
 def index(request):
-    return render(request, 'index.html', {})
+    return render(request, 'index.html')
